55ffbbcd8616575d8095c19975accc67bf1cb2e1
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-06-30 23:20:42 +0200

    Added the contours detector

---
+++ contours_detection.py
@@ -0,0 +1,28 @@
+#Reference: https://www.youtube.com/watch?v=hrwsHlKqBRw
+import cv2
+from preprocessing import preprocess
+
+
+def get_contours(image):
+    img = image
+    image = preprocess(image, 7)
+
+    blurred = cv2.pyrMeanShiftFiltering(image, 31, 91)
+
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+
+    ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    _, contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    print("number of contours is: %d"  %len(contours))
+
+    cv2.drawContours(img, contours, -1, (0, 0, 255), 6)
+
+    cv2.namedWindow("Display", cv2.WINDOW_NORMAL)
+    cv2.imshow("Display", img)
+    cv2.waitKey()
+    return contours
+
+
+image = cv2.imread('training_data/001A1FE001CDDE-0014160-0011C03-0017ED2-001229D0012A16001956E.jpg')
+get_contours(image)
