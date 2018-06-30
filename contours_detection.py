b0787131e5ebe9e5963218be6ddd57e1dc5c5d0a
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-07-01 00:56:27 +0200

    documented the contours detection - fixed typos in preprocessing documentation

--- contours_detection.py
+++ contours_detection.py
@@ -1,29 +1,35 @@
-#Reference: https://www.youtube.com/watch?v=hrwsHlKqBRw
+"""
+This file includes an Algorithm to extract the contours from an image.
+
+Reference: https://www.youtube.com/watch?v=hrwsHlKqBRw
+"""
 import cv2
 from preprocessing import preprocess
 
 
-def get_contours(image):
-    img = image
-    image = preprocess(image, 5)
+def get_contours(image, median_size=5):
+    """
+    :param image: The original image, in which you wanna reduce the noise.
+    :param median_size: the matrix dimensions of the median filter
+    :return: numpy array includes the contous in this image
+    :draw: the original image with the contous detected and drawn on it
+    """
+    image_with_noise = image
+    image = preprocess(image, median_size)
 
     blurred = cv2.pyrMeanShiftFiltering(image, 31, 91)
 
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
     ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-   #ret, threshold = cv2.threshold(gray, 127, 255, 1)
+    #ret, threshold = cv2.threshold(gray, 127, 255, 1)
 
     _, contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     #_, contours, _ = cv2.findContours(threshold,1,2)
 
-    cv2.drawContours(img, contours, -1, (0, 0, 255), 6)
+    cv2.drawContours(image_with_noise, contours, -1, (0, 0, 255), 6)
 
-    cv2.namedWindow("Display", cv2.WINDOW_NORMAL)
-    cv2.imshow("Display", img)
+    cv2.namedWindow("Contours Detection", cv2.WINDOW_NORMAL)
+    cv2.imshow("Contours Detection", image_with_noise)
     cv2.waitKey()
     return contours
-
-
-#image = cv2.imread('training_data/001A1FE001CDDE-0014160-0011C03-0017ED2-001229D0012A16001956E.jpg')
-#print(get_contours(image))
--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,18 @@
-'''
+"""
 This file includes preprocessing phase, to reduce the noise in the picture,
 remove salt and pepper noise, and reduce the strength of the crossing lines.
 
 Reference: https://code.tutsplus.com/tutorials/image-filtering-in-python--cms-29202
-'''
+"""
 import cv2
 
 # Median filter
 def preprocess(image, median_size=5):
-    '''
+    """
     :param image: The original image, in which you wanna reduce the noise.
     :param median_size: the matrix dimensions of the median filter
     :return: The cleaned image
-    '''
+    """
     # It supports maximum 7x7 window!
     median_size = min(median_size, 7)
     # apply the (median_size X median_size) median filter on the image [default 5 x 5]
