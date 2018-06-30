4f00d886b34f52ceec43d79782d08b0dec194fea
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-07-01 00:33:13 +0200

    Enhanced the accuracy of the shape detection and contours detection

--- contours_detection.py
+++ contours_detection.py
@@ -5,16 +5,17 @@ from preprocessing import preprocess
 
 def get_contours(image):
     img = image
-    image = preprocess(image, 7)
+    image = preprocess(image, 5)
 
     blurred = cv2.pyrMeanShiftFiltering(image, 31, 91)
 
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
     ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+   #ret, threshold = cv2.threshold(gray, 127, 255, 1)
 
     _, contours, _ = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    print("number of contours is: %d"  %len(contours))
+    #_, contours, _ = cv2.findContours(threshold,1,2)
 
     cv2.drawContours(img, contours, -1, (0, 0, 255), 6)
 
@@ -24,5 +25,5 @@ def get_contours(image):
     return contours
 
 
-image = cv2.imread('training_data/001A1FE001CDDE-0014160-0011C03-0017ED2-001229D0012A16001956E.jpg')
-get_contours(image)
+#image = cv2.imread('training_data/001A1FE001CDDE-0014160-0011C03-0017ED2-001229D0012A16001956E.jpg')
+#print(get_contours(image))
--- detect_shapes.py
+++ detect_shapes.py
@@ -1,30 +1,37 @@
 import cv2
+from contours_detection import get_contours
+from shape_detector import ShapeDetector
 
-img = cv2.imread('training_data/001A0A00019C2D-0012B08-001930D-0013829-001557A00184D80015C5F.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, thresh = cv2.threshold(gray,127,255,1)
+image = cv2.imread('training_data/001EE370015D7B-001B514-001AF1A-001D2BD-001E41B001061C001D292.jpg')
 
-_, contours, _ = cv2.findContours(thresh,1,2)
+sd = ShapeDetector()
+contours = get_contours(image)
+# loop over the contours
 
-for cnt in contours:
-    approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    if len(approx)==5:
-        print ("pentagon")
-        cv2.drawContours(img,[cnt],0,255,-1)
-    elif len(approx)==3:
-        print ("triangle")
-        cv2.drawContours(img,[cnt],0,(0,255,0),-1)
-    elif len(approx)==4:
-        print ("square")
-        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
-    elif len(approx) == 9:
-        print ("half-circle")
-        cv2.drawContours(img,[cnt],0,(255,255,0),-1)
-    elif len(approx) > 15:
-        print ("circle")
-        cv2.drawContours(img,[cnt],0,(0,255,255),-1)
+#delete the boarder if it was added in the contours
+if contours[-1][0][0][0] == 0 and contours[-1][0][0][1] == 0 and contours[-1][-1][0][1] == 0:
+    contours = contours[0:-1]
 
-cv2.imshow('img',img)
+for c in contours:
+    # compute the center of the contour, then detect the name of the
+    # shape using only the contour
+    M = cv2.moments(c)
+    if M["m00"] <= 0.0:
+        continue
+    cX = int((M["m10"] / M["m00"]))
+    cY = int((M["m01"] / M["m00"]))
+
+    shape = sd.detect(c)
+    print(shape)
+    # multiply the contour (x, y)-coordinates by the resize ratio,
+    # then draw the contours and the name of the shape on the image
+    c = c.astype("float")
+    c = c.astype("int")
+    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+
+cv2.imshow('img',image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
